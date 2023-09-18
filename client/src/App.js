e2e3e0240c3754c2ed0f5fc0ad205769296604d0
RAHUL KHALLAR <khallarrahul@gmail.com>
2023-09-18 11:45:54 -0700

    init setup

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import {useState, useEffect} from 'react'
+
+function App() {
+  const [data, setData] = useState("")
+
+  useEffect(() => {
+    fetch("/members").then(res=> res.json())
+    .then(data => {
+      setData(data) 
+      console.log(data)})
+  }, [])
+  return (
+    <div >
+    </div>
+  );
+}
+
+export default App;

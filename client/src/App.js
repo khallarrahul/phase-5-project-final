8bb96682bc4a44179c544da131425b5b92fb35bb
RAHUL KHALLAR <khallarrahul@gmail.com>
2023-09-18 17:36:17 -0700

    adds navbar, home, navlinks

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,20 @@
-
 import './App.css';
-import {useState, useEffect} from 'react'
+import Login from './Login';
+import Home from './Home';
+import Signup from './Signup';
+import NavBar from './NavBar';
+import { Switch, Route } from 'react-router-dom'
 
 function App() {
-  const [data, setData] = useState("")
-
-  useEffect(() => {
-    fetch("/members").then(res=> res.json())
-    .then(data => {
-      setData(data) 
-      console.log(data)})
-  }, [])
+  
   return (
     <div >
+      <NavBar/>
+      <Switch>
+        <Route exact path="/"><Home/></Route>
+        <Route path="/login"><Login/></Route>
+        <Route path="/signup"><Signup/></Route>
+      </Switch>
     </div>
   );
 }

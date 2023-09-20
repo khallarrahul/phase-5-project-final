6081bb484e13174fb1b31b79a8f0932675b3d544
RAHUL KHALLAR <khallarrahul@gmail.com>
2023-09-20 09:34:02 -0700

    adds error for wrong routing

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,20 @@ import Login from './Login';
 import Home from './Home';
 import Signup from './Signup';
 import NavBar from './NavBar';
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
-  
   return (
-    <div >
-      <NavBar/>
+    <div>
+      <NavBar />
       <Switch>
-        <Route exact path="/"><Home/></Route>
-        <Route path="/login"><Login/></Route>
-        <Route path="/signup"><Signup/></Route>
+        <Route exact path="/"><Home /></Route>
+        <Route path="/login"><Login /></Route>
+        <Route path="/signup"><Signup /></Route>
+        <Route>
+          <h1>The page you are looking for does not exist</h1>
+          <h1>ERROR 404</h1>
+        </Route>
       </Switch>
     </div>
   );
